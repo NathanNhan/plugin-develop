7e5c00058d3de6d371c9bd9f4d18b0fcee882ac4
NathanNhan <44769901+NathanNhan@users.noreply.github.com>
2025-11-16 13:21:10 +0700

    first commit

---
+++ plugin-development.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Plugin Name:       My Plugin Develop
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            John Smith
+ * Author URI:        https://author.example.com/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if(! defined('MPD_PLUGIN_VERSION')) {
+    define('MPD_PLUGIN_VERSION','1.0.0');
+}
+
+if(! defined('MPD_PLUGIN_DIR_PATH')) {
+    define('MPD_PLUGIN_DIR_PATH',plugin_dir_path(__FILE__));
+}
+
+if(! defined('MPD_PLUGIN_DIR_URL')) {
+    define('MPD_PLUGIN_DIR_URL',plugin_dir_url(__FILE__));
+}
+
+function load_assets() {
+    wp_enqueue_style( 'my-css',MPD_PLUGIN_DIR_URL . '/admin/css/style.css' , [], '1.0.0', 'all' );
+    wp_enqueue_script( 'index-js', MPD_PLUGIN_DIR_URL . '/admin/js/index.js' , [], '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'load_assets');
